dc59aadfbc20e04cfa9b1c2e5546df4544f10099
David Daza <drdaza18@gmail.com>
2023-02-06 10:40:29 +0100

    added player class without test

---
+++ src/main/java/com/factoriaf5/kata/Player/Player.java
@@ -0,0 +1,67 @@
+package com.factoriaf5.kata.Player;
+
+import java.util.Random;
+
+public class Player {
+
+    private int point;
+    private String nickName;
+    private Boolean begin;
+
+    public Player(String nickName, Boolean begin) {
+        this.point = 0;
+        this.nickName = nickName;
+        this.begin = begin;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public Boolean getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Boolean begin) {
+        this.begin = begin;
+    }
+
+    public void makeService(Player opponent){
+        /* if(makePoint()) setPoint(getPoint()+1); */
+        if(begin){
+
+            if(makePoint(opponent)) setPoint(getPoint()+1);
+            
+        }
+    }
+
+    public Boolean makePoint(Player opponent){
+        if(!opponent.punchBall()) return true;
+        return false;
+    }
+
+    public Boolean punchBall(){
+        Random numAleatorio = new Random();
+
+        boolean punch = numAleatorio.nextBoolean();
+
+        if(punch) return true;
+
+        return false;
+    }
+
+    
+
+    
+}
---
+++ src/main/java/com/factoriaf5/kata/Score.java
@@ -0,0 +1,18 @@
+package com.factoriaf5.kata;
+
+import com.factoriaf5.kata.Player.Player;
+
+public class Score {
+    private int counter;
+    private Player player;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    
+}
