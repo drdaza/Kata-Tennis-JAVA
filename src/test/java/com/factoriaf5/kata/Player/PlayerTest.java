f02f634e82620e50b0a69ddfba1c5259028119cf
David Daza <drdaza18@gmail.com>
2023-02-07 20:41:54 +0100

    cleanning warnings :D

--- src/test/java/com/factoriaf5/kata/Player/PlayerTest.java
+++ src/test/java/com/factoriaf5/kata/Player/PlayerTest.java
@@ -2,9 +2,9 @@
 package com.factoriaf5.kata.Player;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import org.junit.jupiter.api.Test;
 
